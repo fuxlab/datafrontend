1d440c34b7b9a69318ed8ca8fe76740837fa4585
Frank <frank.cieslik@gmail.com>
2019-06-25 18:14:51 +0000

    Resolve "Image UI Details"

--- dashboard/frontend/webpack.config.js
+++ dashboard/frontend/webpack.config.js
@@ -44,7 +44,10 @@ module.exports = (env = {}) => {
             loader: 'babel-loader',
             options: {
               cacheDirectory: true,
-              presets: ["@babel/preset-env","@babel/preset-react"]
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+              ]
             }
           }
         },
