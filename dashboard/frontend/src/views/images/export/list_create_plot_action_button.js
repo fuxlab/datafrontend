8fd258d607cf003aea01cc66ce9ed138e679228c
Frank Cieslik <frank.cieslik@gmail.com>
2019-07-16 14:33:30 +0200

    merge

--- dashboard/frontend/src/views/images/export/list_create_plot_action_button.js
+++ dashboard/frontend/src/views/images/export/list_create_plot_action_button.js
@@ -6,17 +6,29 @@ import { stringify } from 'query-string';
 class CreatePlotButton extends Component {
     render() {
         const {  basePath, resource, selectedIds, startUndoable, filterValues } = this.props;
-        const params = {
+        
+        const plot_link_original = '/api/image/plot.png?' + stringify({
+            type: 'annotation',
+            ids: [selectedIds]
+        })
+        const plot_link_type = '/api/image/plot.png?' + stringify({
             type: filterValues.type,
             ids: [selectedIds]
-        }
-        const plot_link = '/api/image/plot.png?' + stringify(params)
+        })
+
         return (
-            <Button
-                label="Create Image-Plot"
-                href={plot_link}
-                target='_blank'
-            />
+            <div>
+                <Button
+                    label="Original Image-Plot"
+                    href={plot_link_original}
+                    target='_blank'
+                />
+                <Button
+                    label="Create Image-Plot for Type"
+                    href={plot_link_type}
+                    target='_blank'
+                />
+            </div>
         );
     }
 }
